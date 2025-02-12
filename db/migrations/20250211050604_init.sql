a54192912f249ab495653c601133a9c53eaec8eb
Pavel Chubakov <dev.waylander@gmail.com>
2025-02-12 15:52:56 +0700

    feat: migration for source data

--- db/migrations/20250211050604_init.sql
+++ db/migrations/20250211050604_init.sql
@@ -33,6 +33,19 @@ CREATE TABLE shop."merch" (
 
 CREATE INDEX "merch@name_id_idx" ON shop."merch" (name);
 
+-- Исходные данные
+INSERT INTO shop."merch" (name, price) VALUES
+    ('t-shirt', 80),
+    ('cup', 20),
+    ('book', 50),
+    ('pen', 10),
+    ('powerbank', 200),
+    ('hoody', 300),
+    ('umbrella', 200),
+    ('socks', 10),
+    ('wallet', 50),
+    ('pink-hoody', 500);
+
 -- inventory
 CREATE TABLE shop."inventory" (
     id BIGSERIAL PRIMARY KEY,
